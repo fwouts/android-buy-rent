21e8882d624069749b3b817fa019b818ae2f773d
François Wouts <f@zenc.io>
2021-04-21 23:46:13 +1000

    Introduce makePropertyListing() helper function

--- app/src/androidTest/java/com/fwouts/buyrent/PropertyListInstrumentedTest.kt
+++ app/src/androidTest/java/com/fwouts/buyrent/PropertyListInstrumentedTest.kt
@@ -28,27 +28,37 @@ import org.mockito.kotlin.whenever
 @LargeTest
 class PropertyListInstrumentedTest {
     companion object {
-        val PROPERTY_LISTING_1 = PropertyListing(
+        val PROPERTY_LISTING_1 = makePropertyListing(
             id = 2,
             price = "$20M",
             address = "100 Harris Street, Pyrmont",
             bedroom_count = 0.0f,
             bathroom_count = 12.0f,
             carspace_count = null,
-            media = listOf(Media("foo")),
-            advertiser = Advertiser(
-                images = AdvertiserImages(
-                    logo_url = "foo"
-                )
-            )
         )
-        val PROPERTY_LISTING_2 = PropertyListing(
+        val PROPERTY_LISTING_2 = makePropertyListing(
             id = 1,
             price = "not for sale",
             address = "1 Infinity Loop",
             bedroom_count = 2.5f,
             bathroom_count = 2.0f,
             carspace_count = 1,
+        )
+
+        fun makePropertyListing(
+            id: Long,
+            price: String,
+            address: String? = null,
+            bedroom_count: Float? = null,
+            bathroom_count: Float? = null,
+            carspace_count: Int? = null
+        ) = PropertyListing(
+            id = id,
+            price = price,
+            address = address ?: "address",
+            bedroom_count = bedroom_count ?: 2.5f,
+            bathroom_count = bathroom_count ?: 2.0f,
+            carspace_count = carspace_count,
             media = listOf(Media("foo")),
             advertiser = Advertiser(
                 images = AdvertiserImages(
