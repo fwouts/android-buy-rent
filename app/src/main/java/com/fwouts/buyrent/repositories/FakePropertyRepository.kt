5cde08a6ac1af833cf684251c3a17484dc570d0b
François Wouts <f@zenc.io>
2021-04-21 11:33:38 +1000

    Add loading state

--- app/src/main/java/com/fwouts/buyrent/repositories/FakePropertyRepository.kt
+++ app/src/main/java/com/fwouts/buyrent/repositories/FakePropertyRepository.kt
@@ -2,6 +2,7 @@ package com.fwouts.buyrent.repositories
 
 import com.fwouts.buyrent.domain.Property
 import com.fwouts.buyrent.testing.fixtures.PropertyFixtures
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -9,6 +10,7 @@ import javax.inject.Inject
 class FakePropertyRepository @Inject constructor(): PropertyRepository {
     override fun getList(): Flow<List<Property>> {
         return flow {
+            delay(3000)
             emit(PropertyFixtures.PROPERTIES)
         }
     }
--- app/src/main/java/com/fwouts/buyrent/ui/list/PropertyListFragment.kt
+++ app/src/main/java/com/fwouts/buyrent/ui/list/PropertyListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -29,6 +30,7 @@ class PropertyListFragment : Fragment() {
         listViewModel = ViewModelProvider(this).get(PropertyListViewModel::class.java)
         listViewModel.list.observe(this, Observer {
             adapter.list = it
+            adapter.notifyDataSetChanged()
         })
     }
 
@@ -39,6 +41,10 @@ class PropertyListFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_main, container, false)
         val recyclerView: RecyclerView = root.findViewById(R.id.recycler_view)
         recyclerView.adapter = adapter
+        val progressBar: ProgressBar = root.findViewById(R.id.progress_bar)
+        listViewModel.list.observe(viewLifecycleOwner, Observer {
+            progressBar.visibility = if (it == null) View.VISIBLE else View.GONE
+        })
         return root
     }
 
