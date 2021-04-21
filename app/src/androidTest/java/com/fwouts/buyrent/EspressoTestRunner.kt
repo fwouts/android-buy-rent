0054f068c3b269b3a4fb198764d6963f13b71d46
François Wouts <f@zenc.io>
2021-04-21 21:51:39 +1000

    Add instrumentation tests

---
+++ app/src/androidTest/java/com/fwouts/buyrent/EspressoTestRunner.kt
@@ -0,0 +1,12 @@
+package com.fwouts.buyrent
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class EspressoTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, appName: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
---
+++ app/src/androidTest/java/com/fwouts/buyrent/PropertyListInstrumentedTest.kt
@@ -0,0 +1,114 @@
+package com.fwouts.buyrent
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.fwouts.buyrent.api.*
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnit
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.whenever
+
+@UninstallModules(ApiModule::class)
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class PropertyListInstrumentedTest {
+    companion object {
+        val PROPERTY_LISTING_1 = PropertyListing(
+            id = 2,
+            price = "$20M",
+            address = "100 Harris Street, Pyrmont",
+            bedroom_count = 0.0f,
+            bathroom_count = 12.0f,
+            carspace_count = null,
+            media = listOf(Media("foo")),
+            advertiser = Advertiser(
+                images = AdvertiserImages(
+                    logo_url = "foo"
+                )
+            )
+        )
+        val PROPERTY_LISTING_2 = PropertyListing(
+            id = 1,
+            price = "not for sale",
+            address = "1 Infinity Loop",
+            bedroom_count = 2.5f,
+            bathroom_count = 2.0f,
+            carspace_count = 1,
+            media = listOf(Media("foo")),
+            advertiser = Advertiser(
+                images = AdvertiserImages(
+                    logo_url = "foo"
+                )
+            )
+        )
+    }
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val mockitoRule = MockitoJUnit.testRule(this)
+
+    @BindValue
+    @Mock
+    lateinit var mockApi: BuyRentApi
+
+    @Test
+    fun showsEmptyMessageWhenNoResults() {
+        runBlocking {
+            whenever(mockApi.search(eq(0), any())).thenReturn(SearchResponse(emptyList()))
+        }
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.empty_view)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun showsPaginatedResults() {
+        runBlocking {
+            whenever(mockApi.search(any(), any())).thenReturn(SearchResponse(emptyList()))
+            whenever(mockApi.search(eq(0), any())).thenReturn(
+                SearchResponse(
+                    listOf(
+                        PROPERTY_LISTING_1
+                    )
+                )
+            )
+            whenever(mockApi.search(eq(1), any())).thenReturn(
+                SearchResponse(
+                    listOf(
+                        PROPERTY_LISTING_2
+                    )
+                )
+            )
+        }
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.empty_view)).check(matches(not(isDisplayed())))
+
+        // First property.
+        onView(withText("not for sale")).check(matches(isDisplayed()))
+        onView(withText("1 Infinity Loop")).check(matches(isDisplayed()))
+        onView(withText("2.5 bed, 2 bath, 1 car")).check(matches(isDisplayed()))
+
+        // Second property.
+        onView(withText("$20M")).check(matches(isDisplayed()))
+        onView(withText("100 Harris Street, Pyrmont")).check(matches(isDisplayed()))
+        onView(withText("0 bed, 12 bath")).check(matches(isDisplayed()))
+    }
+
+    // TODO: Test switching between tabs.
+}
