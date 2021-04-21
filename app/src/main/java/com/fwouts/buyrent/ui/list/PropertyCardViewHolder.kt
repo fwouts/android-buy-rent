bfc3ede88ae5472c45ae324eaf4e9d567f1a18c3
François Wouts <f@zenc.io>
2021-04-21 16:52:47 +1000

    Extract out PropertyCardViewHolder

---
+++ app/src/main/java/com/fwouts/buyrent/ui/list/PropertyCardViewHolder.kt
@@ -0,0 +1,47 @@
+package com.fwouts.buyrent.ui.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.fwouts.buyrent.R
+
+class PropertyCardViewHolder(view: View, private val loadingAnimation: CircularProgressDrawable) :
+    RecyclerView.ViewHolder(view) {
+    companion object {
+        fun create(parent: ViewGroup): PropertyCardViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.view_property_card, parent, false)
+            val loadingAnimation = CircularProgressDrawable(parent.context)
+            loadingAnimation.strokeWidth = 5f
+            loadingAnimation.centerRadius = 30f
+            return PropertyCardViewHolder(view, loadingAnimation)
+        }
+    }
+
+    private val price: TextView = view.findViewById(R.id.price)
+    private val description: TextView = view.findViewById(R.id.description)
+    private val address: TextView = view.findViewById(R.id.address)
+    private val image: ImageView = view.findViewById(R.id.image)
+    private val agencyLogo: ImageView = view.findViewById(R.id.agency_logo)
+
+    fun bind(viewModel: PropertyViewModel) {
+        price.text = viewModel.price
+        description.text = viewModel.description
+        address.text = viewModel.address
+        loadingAnimation.start()
+        val glide = Glide.with(itemView)
+        glide.load(viewModel.imageUrl)
+            .centerCrop()
+            .placeholder(loadingAnimation).into(image)
+        glide.load(viewModel.agencyLogoUrl).into(agencyLogo)
+    }
+
+    fun onRecycled() {
+        loadingAnimation.stop()
+    }
+}
--- app/src/main/java/com/fwouts/buyrent/ui/list/PropertyListAdapter.kt
+++ app/src/main/java/com/fwouts/buyrent/ui/list/PropertyListAdapter.kt
@@ -1,53 +1,21 @@
 package com.fwouts.buyrent.ui.list
 
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
-import com.bumptech.glide.Glide
-import com.fwouts.buyrent.R
 
 class PropertyListAdapter :
-    PagingDataAdapter<PropertyViewModel, PropertyListAdapter.ViewHolder>(PropertyViewModel.COMPARATOR) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.view_property_card, parent, false)
-        val loadingAnimation = CircularProgressDrawable(parent.context)
-        loadingAnimation.strokeWidth = 5f
-        loadingAnimation.centerRadius = 30f
-        return ViewHolder(view, loadingAnimation)
+    PagingDataAdapter<PropertyViewModel, PropertyCardViewHolder>(PropertyViewModel.COMPARATOR) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PropertyCardViewHolder {
+        return PropertyCardViewHolder.create(parent)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: PropertyCardViewHolder, position: Int) {
         getItem(position)?.let { viewModel ->
-            with(viewModel) {
-                holder.price.text = price
-                holder.description.text = description
-                holder.address.text = address
-                holder.loadingAnimation.start()
-                val glide = Glide.with(holder.itemView)
-                glide.load(viewModel.imageUrl)
-                    .centerCrop()
-                    .placeholder(holder.loadingAnimation).into(holder.image)
-                glide.load(viewModel.agencyLogoUrl).into(holder.agencyLogo)
-            }
+            holder.bind(viewModel)
         }
     }
 
-    override fun onViewRecycled(holder: ViewHolder) {
-        holder.loadingAnimation.stop()
-    }
-
-    class ViewHolder(view: View, val loadingAnimation: CircularProgressDrawable) :
-        RecyclerView.ViewHolder(view) {
-        val price: TextView = view.findViewById(R.id.price)
-        val description: TextView = view.findViewById(R.id.description)
-        val address: TextView = view.findViewById(R.id.address)
-        val image: ImageView = view.findViewById(R.id.image)
-        val agencyLogo: ImageView = view.findViewById(R.id.agency_logo)
+    override fun onViewRecycled(holder: PropertyCardViewHolder) {
+        holder.onRecycled()
     }
 }
